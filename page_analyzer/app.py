cddfda5224f042fa11cb12ba9bde9b65ad54aae8
Ramil Albakov <ramil.albakov2@gmail.com>
2024-01-16 09:33:59 +0600

    feat: add custom 500, 404 http code handler

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -116,3 +116,13 @@ def url_check(id):
     connection_to_db.close()
 
     return redirect(url_for('url_details', id=id))
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html'), 500
